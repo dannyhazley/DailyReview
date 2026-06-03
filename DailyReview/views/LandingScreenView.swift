12cc268b81897dae1d3dc8f1356ff5fe094e79d2
Danny Hazley <54882850+dannyhazley@users.noreply.github.com>
2026-06-03 16:57:29 +0100

    Home Page, gitignore

--- DailyReview/views/LandingScreenView.swift
+++ DailyReview/views/LandingScreenView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct LandingScreenView: View {
     var body: some View {
-        
+        VStack{
+            Text("What do you want to Review?")
+                .font(Font.largeTitle.bold())
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color.red)
+ 
+            Spacer()
+            
+            ForEach (["Daily", "Weekly", "Monthly"], id: \.self) {reviewType in
+                Button("\(reviewType) Review"){
+                    
+                }.font(Font.title)
+                    .padding()
+                    .foregroundStyle(Color.primary)
+                    
+            }
+            
+            Spacer()
+        }.padding()
     }
 }
 
