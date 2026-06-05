89401eeedeb4cb9bca3b74c56eb2c5137668796b
Danny Hazley <54882850+dannyhazley@users.noreply.github.com>
2026-06-05 11:14:36 +0100

    All inputs wired through to displays from Textfields

---
+++ DailyReview/Model/ReviewFormState.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewFormState.swift
+//  DailyReview
+//
+//  Created by Danny Hazley on 04/06/2026.
+//
+
+import Foundation
---
+++ DailyReview/Views/TextListInputView.swift
@@ -0,0 +1,18 @@
+//
+//  TextListInputView.swift
+//  DailyReview
+//
+//  Created by Danny Hazley on 04/06/2026.
+//
+
+import SwiftUI
+
+struct TextListInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextListInputView()
+}
